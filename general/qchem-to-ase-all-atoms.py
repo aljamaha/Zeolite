f24d7afbbd7bb579ad02852527b80cf17b88f84f
aljamaha <aljamaha@stanford.edu>
2019-12-11 16:42:15 -0800

    Update qchem-to-ase-all-atoms.py
    change comments on qm total atoms input and also prints full atoms traj files

--- general/qchem-to-ase-all-atoms.py
+++ general/qchem-to-ase-all-atoms.py
@@ -3,7 +3,7 @@ from molmod import *
 
 '''
 Objective:    convert qchem output to traj files
-Input format: python strip-qchem-output.py <qchem-output filename> <xyz initial input filename> <total #of qm atoms>
+Input format: python strip-qchem-output.py <qchem-output filename> <xyz initial input filename> <#of qm atoms>
 Output:       xyz files under the folder traj
 '''
 
@@ -17,22 +17,22 @@ if len(sys.argv) !=4:
 'input arguments'
 qout    = sys.argv[1]	#qchem output file name
 xyzfile = sys.argv[2]	#xyz coordinates original input file name
-atoms   = sys.argv[3]   #total # of atoms
+atoms   = sys.argv[3]   #total number of atoms
 
 'read q-chem output file'
 f = open(qout, 'r')
 lines = f.read().split("\n")
 f.close()
 
-os.system('mkdir test')	#data are saved here
-file_number = 0		#for numbering traj files
+os.system('mkdir traj-full-atoms')	#data are saved here
+file_number = 0				#for numbering traj files
 
 for index, line in enumerate(lines):
 	'identify where qm regions are defined'
 	if 'Standard Nuclear Orientation' in line:
 		if ' ----------------------------------------------------------------' in lines[index+int(atoms)+3]:
 			file_number +=1
-			g = open('test/'+str(file_number)+'.xyz','w')
+			g = open('traj-full-atoms/'+str(file_number)+'.xyz','w')
 			g.write(atoms+'\n\n')
 			for i in range(index,index+int(atoms)+3):
 				if i in [index,1+index,2+index]:
@@ -41,4 +41,5 @@ for index, line in enumerate(lines):
 				else:
 					g.write(lines[i][11:]+'\n')
 			g.close()
-os.system('cp test/'+str(file_number)+'.xyz final.xyz')
+
+os.system('cp traj-full-atoms/'+str(file_number)+'.xyz full-atoms.xyz')
