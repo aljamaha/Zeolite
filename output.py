8af1e59064cf4cf784c05a8aaf6c300698936db2
aljamaha <aljamaha@stanford.edu>
2020-01-07 13:17:52 -0800

    Update output.py
    complete revamp. Adds traj files for only those not generated before. It calculates H_qm atoms without a guess. checks if energy is nan and assigns an empty value if that is the case

--- output.py
+++ output.py
@@ -1,11 +1,12 @@
-import os, json
+import os, json, sys
+from ase import io
 
 'Analyze the output of qmmm calculations and exports data to output_data.json'
 
 'Inputs'
 calc_dir = '/home/aljama/CHA/calculations/'	#directory where caluculatiosn are saved
 data_dir = '/home/aljama/CHA/data/'		#directory where data are saved
-scripts_dir = '/home/aljama/scripts/'
+scripts_dir = '/home/aljama/scripts/'		#directory where zeolites scripts are
 
 cwd = os.getcwd()
 
@@ -35,7 +36,10 @@ def extract_energy(output_file):
 		if 'Total energy in the final basis set' in line:
 			step += 1
 			if step == 1:
-				E0 = float(line[-14:])
+				if line[-14:] == 'sis set = -nan':
+					return 'nan'
+				else:
+					E0 = float(line[-14:])
 			energy.append(float(line[-14:])- E0)
 			steps.append(step)
 
@@ -46,7 +50,7 @@ def extract_energy(output_file):
 def calc_status(wd):
 	'''
 	check calcualtion status
-	Output:
+	Output: 
 		status of calculation (complete/incomplete)
 		name of output folder
 	'''
@@ -64,7 +68,39 @@ def calc_status(wd):
 
 	return status, output_folder
 
-folders = folders_list(calc_dir)	#folders in calculations/ directory
+def Al_Si_atoms():
+	'finds number of Al atoms in qm region'
+	atoms = io.read('qm-initial.traj')
+	n_Al, n_Si, n_O = 0, 0, 0
+	for atom in atoms:
+		if atom.symbol == 'Al':
+			n_Al += 1
+		elif atom.symbol == 'Si':
+			n_Si += 1
+		elif atom.symbol == 'O':
+			n_O  += 1
+
+	return n_Al, n_Si, n_O
+
+def H_qm_region(n_O, n_Si, n_Al):
+	'''identifies the number of H atoms in qm region added to justify dangling bonds
+	Inputs: 
+		n_O:  number of O  in qm region
+		n_Si: number of Si in qm region
+		n_Al: number of Al in qm region
+	Outputs:
+		number of additional H in qm region to prevent dangling Si bonds
+	'''
+
+	num_oxygen_bonds = n_O*2	#total number of bonds oxygen can make in QM region
+	num_oxygen_bonds_to_Al = n_Al*4
+	num_oxygen_bonds_to_Si = num_oxygen_bonds - num_oxygen_bonds_to_Al
+	tot_num_Si_bonds_needed = n_Si*4
+	tot_Si_bonds_with_H_needed = tot_num_Si_bonds_needed - 	num_oxygen_bonds_to_Si
+
+	return tot_Si_bonds_with_H_needed 
+	
+folders = folders_list(calc_dir)	#folders in calculations/directory
 
 'open saved json file'
 with open(data_dir+"/data_output.json", "r") as read_file:
@@ -75,41 +111,64 @@ with open(data_dir+"/data.json", "r") as read_file:
 
 'data extraction'
 for folder in folders:
-	tmp_status = '' #temporary set that calc is not complete
+
+	print('calculation: ', folder)
+	tmp_energy = '' #assign an empty initial value
+	
 	if folder in data:
-		if 'status' in data[folder]:
-			if data[folder]['status'] == 'complete':
-				'if it passes those tests, calc is complete and no need for further check'
-				tmp_status = 'complete'
-	if tmp_status == 'complete':
-		'completed calc has been stored'
-		continue
+		'check status of calculation and energy'
+		if 'energy' in data[folder]:
+			tmp_energy = 'extracted'
 	else:
-		'if calculation is incomplete, then proceed to check status'
-		data[folder] = {}
-		os.chdir(calc_dir+'/'+folder)
+		'create new entry if it does not exist'
+		data[folder] = {}	#create new entry in dictionary
 
-		for index, item  in enumerate(folder):
-			if item == '-':
-				'extracts index (used to extract original data)'
-				data[folder]['index'] = folder[0:index]
-				ref = str(folder[0:index])+'.traj'	#reference in original calculation
-				break
+	os.chdir(calc_dir+'/'+folder)
 
-		'extract calculation detail'
-		data[folder]['calc']   = folder[index+1:]
+	if os.path.isfile("qm-initial.traj") == False:
+		'print structure of initial qm region'
+		os.system('cp '+scripts_dir+'/qm_structure.py .')
+		os.system('python qm_structure.py')
+
+	for index, item  in enumerate(folder):
+		if item == '-':
+			'extracts index (used to extract original data)'
+			data[folder]['index'] = folder[0:index]					
+			ref = str(folder[0:index])+'.traj'	#reference in original calculation
+			break
+
+	'name of output file'
+	output_file = calc_status(os.getcwd())[1]
+
+	if tmp_energy != 'extracted':
+		'extract calculation detail/Energy'
+		data[folder]['calc']   = folder[index+1:] 
 		data[folder]['status'] = calc_status(os.getcwd())[0]
-
+		
 		if data[folder]['status'] == 'complete':
 			'extract energy if calculation is complete'
-			output_file = calc_status(os.getcwd())[1]
 			E = extract_energy(output_file)
 			data[folder]['energy'] = E
+			if E == 'nan':
+				data[folder]['status'] = 'incomplete'
+			else:
+				tmp_energy = 'extracted'
 
-			'print traj of the qm region'
-			os.system('python '+scripts_dir+'/convert-qchem-output-to-ase.py '+output_file+' '+'input.xyz'+' '+str(len(data_original[ref]['qm_region'])+16))
+	if tmp_energy == 'extracted':	
+		if os.path.isfile('traj/2.xyz') == False:
+			'print qm traj files if they do not exist'
+			os.system('cp '+scripts_dir+'/convert-qchem-output-to-ase.py .')
+			n_Al,n_Si, n_O = Al_Si_atoms()	#number of Al/Si atoms in qm region
+			if n_Al == 1:
+				H_qm = 12 #H added to qm region to justify MM region
+			elif n_Al == 2:
+				H_qm = H_qm_region(n_O, n_Si, n_Al)  #H added to qm region to justify MM region
+				n_qm = len(data_original[ref]['qm_region']) + H_qm #total atoms in qm region
+				n_qm = str(n_qm)
+				os.system('python convert-qchem-output-to-ase.py '+output_file+' '+'input.xyz'+' '+n_qm)
 
-			'print full traj of all atoms'
+		if os.path.isfile('traj-full-atoms/2.xyz') == False:
+			'print full traj of all atoms if they do not exist'
 			os.system('python '+scripts_dir+'qchem-to-ase-all-atoms.py '+output_file+' '+'input.xyz'+' '+str(data_original[ref]['total_atoms']))
 
 for item in data:
