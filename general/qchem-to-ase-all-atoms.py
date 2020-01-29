f9d4b0b7c81cbc1566a87c75ea2562052ab2962f
aljamaha <aljamaha@stanford.edu>
2020-01-29 11:51:34 -0800

    no need for input arguments

--- general/qchem-to-ase-all-atoms.py
+++ general/qchem-to-ase-all-atoms.py
@@ -9,22 +9,27 @@ Output:       xyz files under the folder traj
 
 'Modified version from Jeroen Van Der Mynsbrugge original script'
 
-if len(sys.argv) !=4:
-	'raise an error when less than two arguments are provided'
-	sys.stderr.write("Wrong input format\nInput format: python strip-qchem-output.py <qchem-output file name> <xyz initial input> <total #of atoms>\n")
-	quit()
+try:
+	'input arguments'
+	atoms   = sys.argv[3]   #total number of atoms
+	with open("data_dir.json", "r") as read_file:
+    		data = json.load(read_file)
+		atoms = data['total_atoms']
+except:
+	print('dir_data.json is not found')
+	exit()
+try:
+	'read q-chem output file'
+	f = open('opt.out', 'r')
+	lines = f.read().split("\n")
+	f.close()
+except:
+	print('opt.out file is not found')
+	exit()
 
-'input arguments'
-qout    = sys.argv[1]	#qchem output file name
-xyzfile = sys.argv[2]	#xyz coordinates original input file name
-atoms   = sys.argv[3]   #total number of atoms
+if os.path.exists('traj-full atoms') == False:
+	os.system('mkdir traj-full-atoms')	#data are saved here
 
-'read q-chem output file'
-f = open(qout, 'r')
-lines = f.read().split("\n")
-f.close()
-
-os.system('mkdir traj-full-atoms')	#data are saved here
 file_number = 0				#for numbering traj files
 
 for index, line in enumerate(lines):
