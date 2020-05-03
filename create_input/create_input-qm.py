1bc2817611bc52ec4e97f6f16cf72d2a1c309da6
aljamaha <aljamaha@stanford.edu>
2020-05-02 19:01:46 -0700

    adjustments to accommodate BEA and allow generation of only qm region

--- create_input/create_input-qm.py
+++ create_input/create_input-qm.py
@@ -5,13 +5,14 @@ from ase import io
 import json
 
 '''
-Running calculations on selected strucutres. Provide Inputs below
+create input files with only qm region (to compare nuclear repulsion energies)
 '''
+
 'Inputs'
-calc         = [510,2000]	#if continous list, input first and last. Otherwise, input individual entries
+calc         = [1,2000]	#if continous list, input first and last. Otherwise, input individual entries
 multiplicity = 1	#multiplicity of the structure
 job_type     = 'opt' 	#either sp or opt
-dir_name     = 'BEA'	#name of the parent dir
+dir_name     = 'BEA/BEA-full-Si'	#name of the parent dir
 zeolite      = 'BEA'	#zeolite name
 
 'Inputs (rarely need a change)'
@@ -89,9 +90,9 @@ def opt_section(fixed_atoms):
 		f.write(str(n)+'\tXYZ\n')
 	f.write('endfixed\n$end\n\n')
 
-def molecules_section():
+def molecules_section(charge):
 	'writes details of the #molecule section'
-	f.write('$molecule\n0  '+str(multiplicity)+'\n')
+	f.write('$molecule\n'+str(charge)+'  '+str(multiplicity)+'\n')
 	if os.path.getsize('tmp') == 0:
 		print('Connectivity_NL results in an empty $mol section')
 		exit()
@@ -103,16 +104,10 @@ def molecules_section():
 
 def qm_fixed_regions(traj, data, struc_dir):
 	'qm region'
-	qm_atoms    = data[traj+'.traj']['qm_region']
-	fixed_atoms = []
-	atoms       = io.read(struc_dir+'/'+traj+'.traj')
-	for index, item in enumerate(atoms):
-		if index in qm_atoms:
-			continue
-		else:
-			fixed_atoms.append(str(index))
+	fixed_atoms, qm_atoms = [],[]
+	for atom in atoms:
+		qm_atoms.append(atom.index)	
 	return fixed_atoms, qm_atoms
-
 
 def Al_Si_atoms():
 	'finds number of Al/O/Si atoms in qm region'
@@ -168,77 +163,59 @@ if os.path.exists(calc_dir) is not True:
 
 for calc in calculations:
 	
-	print(calc)
-	status = ''
-
-	if job_type == 'sp':
-		'check opt calc is done before generating sp folders'
-		try:
-			data_output[calc+'-opt-omegab97x-d-def2-svp']['energy'] - 0
-		except:
-			print(calc, 'opt calculation is incomplete')
-			status = 'incomplete'
+		print(calc)
 		
-	if status == '':
-		
+		'check calc is available'
 		try:
 			data[calc+'.traj']['reference']
 		except:
 			print('this is likely a duplicate that has been deleted')
 			continue
-		
 			
-		'avoid generating input for sp calculations where opt is incomplete'	
+		'create folder'
 		if os.path.exists(calc_dir+'/'+calc+'-'+details+'-ref-'+data[calc+'.traj']['reference']) is not True:
 			os.system('mkdir '+calc_dir+'/'+calc+'-'+details+'-ref-'+data[calc+'.traj']['reference'])
 		os.chdir(calc_dir+'/'+calc+'-'+details+'-ref-'+data[calc+'.traj']['reference'])
 
-
-		if basis == 'def2-sv(p)':
-			os.system('cp '+struc_dir+'/'+calc+'.traj input.traj')
-			atoms = io.read('input.traj')
-			n_atoms = len(atoms)
-			atoms.write('input.xyz')
-			clean_input('input.xyz')
-			
-		elif basis == 'def2-tzvpd':
-			os.system('cp '+calc_dir+'/'+calc+'-opt-omegab97x-d-def2-svp/full-atoms.xyz input.xyz')
-			clean_input('input.xyz')
+		'copy main input traj'
+		os.system('cp '+struc_dir+'/'+calc+'.traj input.traj')
+		atoms = io.read('input.traj')
+		atoms.write('input.xyz')
 
 		try:
 			os.system('cp '+data_dir+'/'+zeolite+'-NL.json .')
 		except:
 			print('original structure of zeolite-NL.json does not exist')
-			exit()
 
-		os.system('cp '+create_input_dir+'/connectivity_NL.py .')
-		fixed_atoms, qm_atoms = qm_fixed_regions(calc, data, struc_dir)
-		os.system('python connectivity_NL.py input.xyz '+str(qm_atoms)+' '+zeolite+' > tmp')
-
+		'print initial qm structure'
 		with open("dir_data.json", "w") as write_file:
 			json.dump(data[calc+'.traj'], write_file, indent=4)
+		os.system('cp '+scripts_dir+'/qm_structure.py .')
+		os.system('python qm_structure.py')	
+		atoms = io.read('qm-initial.traj')
+		atoms.write('input.xyz')
+
+		'connectivity'
+		os.system('cp '+create_input_dir+'/connectivity_NL.py .')
+		fixed_atoms, qm_atoms = qm_fixed_regions(calc, data, struc_dir)
+		n_atoms = len(atoms)
+		os.system('python connectivity_NL.py input.xyz '+str(qm_atoms)+' '+zeolite+' > tmp')
+
+		'charge'
+		n_Al,n_Si, n_O = Al_Si_atoms()
+		if n_Al == 1:
+			charge = '1'	
+		elif n_Al == 2:
+			charge = '2'
 
 		'''writing opt.in'''
 		f = open('opt.in','w')
 		rem_section()
 		qm_atoms_section(qm_atoms)
-		comments_section()
 		ff_parameters()
 		opt_section(fixed_atoms)
-		molecules_section()
+		molecules_section(charge)
 		f.close()
-
-		'print initial qm structure'
-		os.system('cp '+scripts_dir+'/qm_structure.py .')
-
-		os.system('python qm_structure.py')	
-		'add to dir_data number of QMMM atoms'	
-		n_Al,n_Si, n_O = Al_Si_atoms()
-		if n_Al == 1:
-			H_qm = 12 #H added to qm region to justify MM region
-		elif n_Al == 2:
-			H_qm = H_qm_region(n_O, n_Si, n_Al)  #H added to qm region to justify MM region
-		data[calc+'.traj']['QMMM length'] = len(data[calc+'.traj']['qm_region']) + H_qm #total atoms in qm region
 
 		with open("dir_data.json", "w") as write_file:
 			json.dump(data[calc+'.traj'], write_file, indent=4)
