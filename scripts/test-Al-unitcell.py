8b37ab73bd4bdc26aaf326f41d9dc96d51c75597
aljamaha <aljamaha@stanford.edu>
2020-05-07 19:44:24 -0700

    shuffling scripts

--- scripts/test-Al-unitcell.py
+++ scripts/test-Al-unitcell.py
@@ -1,24 +1,19 @@
 import os
 from ase import io
 
-run  = True
-traj = False
-cores = '1'
-start = 20
-end   = 2000
-calc_type = 'opt'
-
-cwd = os.getcwd()
+start = 1
+end   = 278
+cwd = '/home/aljama/BEA/original/calculations'
 
 for i in range(start,end):
-		i = str(i)
-		try:
+	i = str(i)
+	try:
 			os.chdir(cwd+'/'+i+'-opt-omegab97x-d-def2-svp-ref-'+i+'.traj')	
 			atoms = io.read('input.xyz')
 			for atom in atoms:
 				if atom.symbol == 'Al':
 					if atom.index > 192:
 						print('Warning!', i)
-		except:
-			pass
+	except:
+			print('Failed!', i)
 		
