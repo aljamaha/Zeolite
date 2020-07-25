b24785232f5d1c7a702d40e2f52376c489cdaa90
aljamaha <aljamaha@stanford.edu>
2020-07-24 19:10:33 -0700

    updated to reflect opt hGGA calc that dont have sp counterpart

--- scripts/sp-for-every-opt.py
+++ scripts/sp-for-every-opt.py
@@ -3,7 +3,7 @@ import os, json
 'checks for every opt folder, there is a sp folder'
 
 'Inputs'
-local_dir = 'BEA/H/'
+local_dir = 'BEA/Pd1/'
 exchange  = 'omega'
 
 'General Inputs'
@@ -15,28 +15,39 @@ os.chdir(wd+local_dir+'/calculations')
 os.system("ls > tmp")
 folders = [line.rstrip('\n') for line in open('tmp')]  #this is the best way
 
-'load data'
-with open(wd+'/'+local_dir+"/data/data.json") as read:
-	data = json.load(read)
-with open(wd+'/'+local_dir+"/data/data_output.json") as read:
-	data_output = json.load(read)
+def comp():
+	'check opt.out if calculation has completed'
+	tmp = False
+	try:
+		if os.path.exists('opt.out'):
+			os.system('tail opt.out > tmp')
+			lines =  [line.rstrip('\n') for line in open('tmp')]
+			os.system('rm tmp')
+			for line in lines:
+				if 'Thank you very much for using Q-Chem' in line:
+					tmp = True
+					break
+	except:
+		pass
+	return tmp
 
 'check sp calc available for each sp'
 for calc in folders:
 	if 'opt' in calc and exchange in calc:
 		index = calc[0:calc.find('-')]	#calc index
-		if data_output[calc]['status'] == 'complete':
-			tmp = False
+		os.chdir(wd+local_dir+'/calculations/'+calc)
+		tmp = False
+		if comp() == True:
 			for item in folders:
 				if item[0:calc.find('-')] == index and 'sp' in item and exchange in item:
 					'sp calc is available!'
 					tmp = True
+					#print(calc, 'Done')
 					break
 			if tmp == False:
 				'no sp calc!'
-				if data[index+'.traj']['Al'] == 1:
-					print(calc, 'sp not available!')
-					aggregate.append(index)
+				print(calc, 'sp not available!')
+				aggregate.append(index)
 
 print(aggregate)
 				
