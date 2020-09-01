09f6a529009098e6bdd51eab6bc5a2070ced9e11
aljamaha <aljamaha@stanford.edu>
2020-09-01 11:44:50 -0700

    labels based on oxygens neighboring cation

--- results-analysis/results-Pd1.py
+++ results-analysis/results-Pd1.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from ase import atoms, io
 from copy import deepcopy
 from functions import *
+import numpy as np
+from oxygen_stable import *
 
 '''
 Results Analysis
@@ -12,13 +14,10 @@ Results Analysis
 plotting    	    = False		#if True, plot results for each reference structure
 sorted_plot	    = True		#if True, bar plots of energies is sorted from lowest to highest
 plt_ref_label	    = True		#if True, add label of the reference to the overall plot
-dir_Pd		    = 'BEA/Pd1'		#name of dir where the calculations are saved
-dir_H		    = 'BEA/H'	#name of directory where comensating protons are saved
+dir_Pd		    = 'BEA/Pd1/'		#name of dir where the calculations are saved
+dir_H		    = 'BEA/H/'	#name of directory where comensating protons are saved
 exchange	    = 'omega'
 calc_type	    = 'sp' 		#opt or sp
-#traj		    = ['34.traj', '36.traj', '39.traj', '41.traj', '42.traj', '63.traj', '64.traj', '65.traj', '67.traj', '69.traj', '71.traj', '78.traj', '2.traj', '84.traj', '87.traj', '3.traj', '95.traj', '98.traj', '100.traj', '101.traj', '4.traj', '5.traj', '120.traj', '6.traj', '127.traj', '130.traj', '131.traj', '132.traj', '134.traj', '7.traj', '8.traj', '148.traj', '158.traj', '162.traj', '163.traj', '165.traj', '10.traj', '167.traj', '11.traj', '186.traj', '195.traj', '200.traj', '204.traj', '206.traj', '211.traj', '212.traj', '234.traj']	#save the top 5 candidates in those traj files
-
-same_side = ['101.traj']
 
 'Directory names'
 data_dir    = '/home/aljama/'+dir_Pd+'/data/'			#dir where json data are saved
@@ -30,21 +29,15 @@ candidates  = []						#run hGGA calc on those
 'Load data from json files'
 with open(data_dir+"data_output.json", "r") as read_file:
     data_output = json.load(read_file)
-
 with open(data_dir+"data.json", "r") as read_file:
     data_original = json.load(read_file)
 
-with open(H_data+'/T-atoms.json','r') as read:
-	data_T = json.load(read)
-
 'accumulate reference entries (templates from which calculations were created and run)'
 references = {} #references for data
-
 for item in data_original:
 	'entries in references dictionary'
 	if data_original[item]['reference'] not in references:
 		references[data_original[item]['reference']] = []
-
 for ref in references:
 	'name of folders that share same reference'
 	for item in data_original:
@@ -54,19 +47,11 @@ for ref in references:
 
 'accumulate traj files'
 Pd_H_d, Pd_H_d_all, minimum = [],[],{}	#saves minimum energy for each reference [minimum['3.traj'] = 22.traj]
-Al_distance = {}
+Al_distance, n_O,n = {},{},{}
 for ref in references:
 
-	#print(ref)
-	#print(len(data_original[ref]['qm_region'])+2,'\n')
-
 	'loop over each reference'
-	x_pos, E, first_item, O_d, label = [],[], True, [],[]	#For plotting purposes
-
-	#if os.path.isdir(results_dir+ref) == False:
-	#	'create folder if it does not exist'
-	#	os.system('mkdir '+results_dir+ref)
-	#os.chdir(results_dir+ref)
+	x_pos, E, first_item, O_d, label, candidates = [],[], True, [],[],[]	#For plotting purposes
 
 	for item in references[ref]:
 		'each item under reference'
@@ -77,18 +62,20 @@ for ref in references:
 			try:
 				if data_output[data_output_entry]['status'] == 'complete':
 					'check calc is completed, then copy traj files to new folder'
-					#os.system('cp '+calc_dir+'/'+data_output_entry+'/qm-initial.traj '+item[0:-5]+'.traj')
 					x_pos.append(int(index))	#x-asis position
 					if first_item == True:
 						E_ref = data_output[data_output_entry]['energy']
 					E.append( (data_output[data_output_entry]['energy']- E_ref)*27.2114 ) #convert from Hartree to e. values of energies for y-axis
-					#print(data_output[data_output_entry]['energy'])
 					first_item = False
 					label.append(index)
 
 					'Al-Al distance'
 					atoms = io.read(calc_dir+data_output_entry+'/qm-initial.traj')
 					Al_distance[ref], n_Al = Al_Al(atoms)
+
+					'# of oxygens next to Al'
+					n[item] = cation_n_O('Pd',atoms,cutoff=3.05)
+
 					'O-O distance'
 					#O_O_distance = O_O(atoms, n_Al)
 					#O_d.append( round(O_O_distance,3) )
@@ -102,11 +89,7 @@ for ref in references:
 		if len(E) >0: #avoid references not calculated yet
 			'bar plot (sorted)'
 			new_x, new_E, x_pts = sort(x_pos, E)
-			#if ref in traj:
-			#	'aggreagte resuts to run hGGA calc'
-			#	for k in new_x[0:5]:
-			#		candidates.append(k)
-			print(ref, len(new_x), new_x[0:6])
+			print(ref, len(new_x), new_x[0:13])
 			plt.bar(x_pts, new_E, align='center', alpha=1)
 			plt.xticks(x_pts, new_x)
 			plt.ylabel('Energy (eV)')
@@ -123,15 +106,10 @@ for ref in references:
 			plt.bar(x_pos, E, align='center', alpha=1)
 			plt.xticks(x_pos, x_pos)
 			plt.ylabel('Energy (eV)')
-			plt.show()
+			if plotting  == True:
+				plt.show()
 
 	'''
-		plt.plot(O_d, E, 'o', markersize=6)
-		plt.xlabel('O-O Distance (A)', fontsize = 10)
-		plt.ylabel('Energy (eV)', fontsize = 10)
-		for i, lab in enumerate(label):
-			plt.text(O_d[i], E[i], lab)
-		plt.show()
 		plt.plot(Pd_H_d, E, 'o', markersize=6)
 		plt.xlabel('Pd-H Distance (A)', fontsize = 10)
 		plt.ylabel('Energy (eV)', fontsize = 10)
@@ -140,83 +118,118 @@ for ref in references:
 		plt.show()
 	'''
 
-E,E_label,ref_label, first_item = [],[],{}, True
-coloring,shade,edge = {},{},{}
 '''Plot rxn energy [bar plot]'''
+E,E_label,ref_label, first_item = [],[],{}, True
+coloring,shade,edge,MR4 = {},{},{},{}
+
 for entry in minimum:
 	if minimum[entry] != '':
 		ref_H = data_original[entry]['reference'] #reference for 16 H calculations
 		zeolite_H = min_H(ref_H, H_data, calc_type)
 		if zeolite_H != 0:
-				'zeolite_H == 0 means that some calc are incomplete'
-				data_output_entry = calc_index(str(minimum[entry]), data_output, exchange, calc_type) #check corresponding name in data_output
-				E_qmmm = data_output[data_output_entry]['energy']
-				E_rxn = rxn_energy(E_qmmm, zeolite_H, 1)
-				#print(entry, round(E_rxn,2))
+			'zeolite_H == 0 means that some calc are incomplete'
+			data_output_entry = calc_index(str(minimum[entry]), data_output, exchange, calc_type) #check corresponding name in data_output
+			E_qmmm = data_output[data_output_entry]['energy']
+			E_rxn = rxn_energy(E_qmmm, zeolite_H, 1)
 			
-				#if first_item == True:
-				#	E_ref = deepcopy(E_rxn)
-				#	first_item = False
-				#E.append(E_rxn - E_ref)
-				E.append(E_rxn)
-				E_label.append(minimum[entry])
-				ref_label[minimum[entry]] = entry
-				if data_original[entry]['Al'] == 1:
-					coloring[entry] = 'y'
-					edge[entry]     = 'y'
-				elif data_original[entry]['Al-Al MR'] == [5,5]:
-					coloring[entry] = 'r'
-					edge[entry]     = 'r'
-					#print(data_original[entry]['Al-Al MR'])	
-				elif data_original[entry]['Al-Al MR'] == [6,6]:
-					coloring[entry] = 'b'
-					edge[entry]     = 'b'
-					#print(data_original[entry]['Al-Al MR'])
-				elif data_original[entry]['Al-Al MR'] == [4,4]:
-					#print(data_original[entry]['Al-Al MR'])
-					coloring[entry] = 'g'
-					edge[entry]     = 'g'
-				else:
-					coloring[entry] = 'c'
-				if data_original[entry]['N'] == 'NNN':
-					shade[entry] = '**'
-				else:
-					shade[entry] = ''
+			E.append(E_rxn)
+			E_label.append(minimum[entry])
+			ref_label[minimum[entry]] = entry
+			if data_original[entry]['Al'] == 1:
+				coloring[entry] = 'y'
+				edge[entry]     = 'y'
+			elif data_original[entry]['Al-Al MR'] == [5,5]:
+				coloring[entry] = 'r'
+				edge[entry]     = 'r'
+			elif data_original[entry]['Al-Al MR'] == [6,6]:
+				coloring[entry] = 'b'
+				edge[entry]     = 'b'
+			elif data_original[entry]['Al-Al MR'] == [4,4]:
+				coloring[entry] = 'g'
+				edge[entry]     = 'g'
+			else:
+				coloring[entry] = 'c'
+			if data_original[entry]['N'] == 'NNN':
+				shade[entry] = '**'
+			else:
+				shade[entry] = ''
+			#if data_original[entry]['Al MR']['4']>2:
+			#	MR4[entry] = True
+					
 		#else:
 		#	#print('{} H calculations are incomplete'.format(entry))
 plt.clf()	#clear plot
 
 'Overall Pd Rxn energy plot'
 new_x, new_E, x_pts = sort(E_label, E)
-#T_atom   = [1119, 1124, 1129, 1134, 1139, 1144, 1149, 1155, 1158]
-#for T in T_atom:
-for index, item in enumerate(new_E):
-	ref = ref_label[new_x[index]]
-	#if ref_label[new_x[index]] in data_T[str(T)]:
-	#	plt.bar(x_pts[index], new_E[index],color=coloring[ref_label[new_x[index]]], edgecolor='k', linewidth=4,align='center', alpha=1)
-	#else:
-	#if coloring[ref_label[new_x[index]]] == 'r':
-	if coloring[ref] == 'c' and ref in same_side:
-			plt.bar(x_pts[index], new_E[index],color=coloring[ref], edgecolor='k', linewidth=4,align='center', alpha=1)
-	else:
-		plt.bar(x_pts[index], new_E[index],color=coloring[ref], hatch=shade[ref],align='center', alpha=0.9)
-	if plt_ref_label == True:
-		plt.text(x_pts[index], min(new_E)-0.1, ref,  color='k',rotation = 90, fontsize=12)	
+'''
+T_atom   = [1119, 1124, 1129, 1134, 1139, 1144, 1149, 1155, 1158]
+for T in T_atom:
+'''
 
-plt.ylim([-3.1, -2.5])
+k = 0
+ref_list = []
+for index, item in enumerate(new_E):
+	k+=1
+	ref = ref_label[new_x[index]]
+	ref_list.append(ref)
+	name = str(new_x[index])+'.traj'
+
+	'''
+		if ref in data_T[str(T)]:
+			plt.bar(x_pts[index], new_E[index],color=coloring[ref_label[new_x[index]]], edgecolor='k', linewidth=4,align='center', alpha=1)	
+		else:
+			plt.bar(x_pts[index], new_E[index],color=coloring[ref_label[new_x[index]]], linewidth=4,align='center', alpha=1)
+	'''
+
+	try:
+
+		#plt.bar(x_pts[index], new_E[index], color=coloring[ref], edgecolor='k', linewidth=4,align='center', alpha=1)
+		#plt.bar(x_pts[index], new_E[index], color=coloring[ref], hatch=shade[ref], align='center', alpha=0.9)	
+		if n[name] == 4:
+			plt.bar(x_pts[index], new_E[index], color='k', hatch=shade[ref], align='center', alpha=0.9)
+			print(new_x[index], n[name])
+		elif n[name] == 3:
+			plt.bar(x_pts[index], new_E[index], color='r', hatch=shade[ref], align='center', alpha=0.9)
+			print(new_x[index], n[name])
+		else:
+			plt.bar(x_pts[index], new_E[index], color='b', hatch=shade[ref], align='center', alpha=0.9)
+		if plt_ref_label == True:
+			#plt.text(x_pts[index], min(new_E)-0.1, ref,  color='k',rotation = 90, fontsize=12)
+			plt.text(x_pts[index], min(new_E)+0.1, n_O[ref],  color='k',rotation = 90, fontsize=12)
+	except:
+		print('Failed', name)
+		pass
+			
+
+
+plt.ylim([np.min(new_E)-0.1, np.max(new_E)+0.1])
 plt.xticks(x_pts, new_x, rotation = 90)
 plt.ylabel('Energy (eV)')
 plt.show()
 plt.clf()
 
+print('ref list', ref_list)
+print('energies', new_E)
+
 'Al-Al Distance'
 for index, item in enumerate(new_E):
 	ref = ref_label[new_x[index]]
+
+	#try:
+	#	if MR4[ref] == True:
+	#		plt.plot(Al_distance[ref], new_E[index],'sk',markersize=9)
+	#except:
+	#	pass
+
 	try:
+		#if ref in inaccessible:
+		#	plt.plot(Al_distance[ref], new_E[index],'sk',markersize=9)
+
 		if shade[ref] == '**':
-			plt.plot(Al_distance[ref], new_E[index],coloring[ref]+'^')
+			plt.plot(Al_distance[ref], new_E[index],coloring[ref]+'^',markersize=6)
 		else:
-			plt.plot(Al_distance[ref], new_E[index],coloring[ref]+'o')
+			plt.plot(Al_distance[ref], new_E[index],coloring[ref]+'o',markersize=6)
 	except:
 		pass
 
