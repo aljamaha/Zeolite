866b7eb973452bd4606a4c9c3f4c854e5b7a9a27
aljamaha <aljamaha@stanford.edu>
2020-09-08 10:40:00 -0700

    second cutoff is introduced

--- results-analysis/oxygen_stable.py
+++ results-analysis/oxygen_stable.py
@@ -2,16 +2,17 @@ from ase import io
 from molmod import *
 import os
 
-def cation_n_O(cation_name, atoms, cutoff=2.5):
+def cation_n_O(cation_name, atoms, cutoff1=2.5, cutoff2=4.4):
 	'''
-	Returns number of oxygens that are within cutoff of cation
+	Returns number of oxygens that are within cutoff of cation 
 	Inputs:
 		cation_name - string of the name of the cation (assuming only one exist)
 		atoms       - ase atoms object
-		cutoff      - cutoff ditance between oxygen and cation
+		cutoff1      - cutoff ditance between oxygen and cation
+		cutoff2      - cutoff ditance between oxygen and cation
 	Output:
-		O_Si	    - # of oxygen atoms next to Si
-		O_Al	    - # of oxygen atoms next to Al
+		O_Si	    - # of oxygen atoms next to Si (<cutoff2)
+		O_Al	    - # of oxygen atoms next to Al (<cutoff1)
 		distances   - distance of oxygen atoms to cation
 	'''
 
@@ -38,15 +39,23 @@ def cation_n_O(cation_name, atoms, cutoff=2.5):
 			cation_index = atom.index
 			break
 
-	'find oxygens next to Si and Al'
+	'find oxygens next to Si and Al (within the first cutoff)'
 	for atom in atoms:
 		if atom.symbol == 'O':
 			d = atoms.get_distance(atom.index, cation_index) 
-			if d < cutoff:
+			if d < cutoff1:
 				distances.append(round(d,1))
 				if atom.index in O:
 					O_Al+=1
 				else:
 					O_Si+=1
+
+	'find oxygens next to Si and Al (between cutoff1 and cutoff2'
+	for atom in atoms:
+		if atom.symbol == 'O':
+			d = atoms.get_distance(atom.index, cation_index) 
+			if d > cutoff1 and d < cutoff2:
+				distances.append(round(d,1))
+				O_Si+=1
 
 	return O_Al, O_Si, distances
